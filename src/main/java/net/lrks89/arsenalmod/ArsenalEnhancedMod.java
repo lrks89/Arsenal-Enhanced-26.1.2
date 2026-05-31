0ded7fcaadb027bc13c28e932202fe6835504410
lrks89 <kendoka89@gmail.com>
2026-05-30 22:56:24 -0400

    Initial Commit

---
+++ src/main/java/net/lrks89/arsenalmod/ArsenalEnhancedMod.java
@@ -0,0 +1,16 @@
+package net.lrks89.arsenalmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArsenalEnhancedMod implements ModInitializer {
+	public static final String MOD_ID = "arsenalmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
